796fd657f8b940cc19ad736d68499f14e3501ba1
gotoshake <317472881@qq.com>
2013-09-02 17:07:31 +0800

    add debug

--- zz/scripts/main_debug.lua
+++ zz/scripts/main_debug.lua
@@ -1,3 +1,6 @@
+local initconnection = require("debugger")
+initconnection('127.0.0.1', 10000, 'luaidekey')
+
 -- for CCLuaEngine
 function __G__TRACKBACK__(errorMessage)
     CCLuaLog("----------------------------------------")
