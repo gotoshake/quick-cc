1665676fae2becb1da7f0f0784f762a946ffef59
gotoshake <317472881@qq.com>
2014-03-31 14:55:59 +0800

    .

--- player/sources/AppDelegate.h
+++ player/sources/AppDelegate.h
@@ -2,12 +2,15 @@
 #ifndef  _APP_DELEGATE_H_
 #define  _APP_DELEGATE_H_
 
+#include "cocoa/CCObject.h"
 #include "CCApplication.h"
 #include "SimulatorConfig.h"
 
+class StartupCall;
+
 /**
  @brief    The cocos2d Application.
- 
+
  The reason for implement as private inheritance is to hide some interface call by CCDirector.
  */
 class  AppDelegate : public cocos2d::CCApplication
@@ -15,20 +18,20 @@ class  AppDelegate : public cocos2d::CCApplication
 public:
     AppDelegate();
     virtual ~AppDelegate();
-    
+
     /**
      @brief    Implement CCDirector and CCScene init code here.
      @return true    Initialize success, app continue.
      @return false   Initialize failed, app terminate.
      */
     virtual bool applicationDidFinishLaunching();
-    
+
     /**
      @brief  The function be called when the application enter background
      @param  the pointer of the application
      */
     virtual void applicationDidEnterBackground();
-    
+
     /**
      @brief  The function be called when the application enter foreground
      @param  the pointer of the application
@@ -39,6 +42,18 @@ public:
 
 private:
     ProjectConfig m_projectConfig;
+
+    friend class StartupCall;
+};
+
+class StartupCall : public CCObject
+{
+public:
+    static StartupCall *create(AppDelegate *app);
+    void startup();
+
+private:
+    AppDelegate *m_app;
 };
 
 #endif // _APP_DELEGATE_H_
